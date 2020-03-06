a850345aafbb5910947d32882809bd0142ff173c
Jeffrey C. Ollie <jeff@ocjtech.us>
2020-03-06 11:55:17 -0600

    cleanups

--- src/main.rs
+++ src/main.rs
@@ -1,52 +1,62 @@
-// This program is free software: you can redistribute it and/or modify
+// This file is part of rts.
+//
+// rts is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
 //
-// This program is distributed in the hope that it will be useful,
+// rts is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
 //
 // You should have received a copy of the GNU General Public License
-// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+// along with this rts.  If not, see <https://www.gnu.org/licenses/>.
 
 extern crate scoped_threadpool;
 extern crate chrono;
 
 use std::process::{Command, Stdio};
-use std::sync::{Arc, Mutex};
-use std::io;
-use std::io::Write;
-use std::io::Read;
+use std::io::{self, Read, Write};
 use std::vec::Vec;
 use std::env;
 use chrono::Utc;
-
-
 use scoped_threadpool::Pool;
 
-fn outputter(marker: &[u8; 1], output: Arc<Mutex<dyn Write>>, input: &mut dyn Read) {
+fn outputter(marker: &[u8; 1], input: &mut dyn Read) {
+    let output = io::stderr();
     let mut vec = Vec::new();
     let mut start = Utc::now();
 
     loop {
         let mut buf = [0; 1];
         let rsize = input.read(&mut buf).expect("can't read stdout");
-        if rsize == 0 { break; }
+        if rsize == 0 {
+            if vec.len() != 0 {
+                let mut handle = output.lock();
+                handle.write(marker).unwrap();
+                handle.write(b" ").unwrap();
+                handle.write(start.to_rfc3339().as_bytes()).unwrap();
+                handle.write(b" ").unwrap();
+                handle.write(vec.as_slice()).unwrap();
+                handle.write(b"\n").unwrap();
+                handle.flush().unwrap();
+            }
+            break;
+        }
         if vec.len() == 0 {
             start = Utc::now();
         }
         vec.push(buf[0]);
         if buf[0] == 10 {
             {
-                let mut output = output.lock().unwrap();
-                output.write(marker).unwrap();
-                output.write(b" ").unwrap();
-                output.write(start.to_rfc3339().as_bytes()).unwrap();
-                output.write(b" ").unwrap();
-                output.write(vec.as_slice()).unwrap();
-                output.flush().unwrap();
+                let mut handle = output.lock();
+                handle.write(marker).unwrap();
+                handle.write(b" ").unwrap();
+                handle.write(start.to_rfc3339().as_bytes()).unwrap();
+                handle.write(b" ").unwrap();
+                handle.write(vec.as_slice()).unwrap();
+                handle.flush().unwrap();
             }
             vec = Vec::new();
         }
@@ -58,31 +68,27 @@ fn main() {
 
     args.remove(0);
 
-    let mut cmd =
+    let mut child =
         Command::new(args.remove(0))
         .args(args)
         .stdin(Stdio::null())
         .stdout(Stdio::piped())
         .stderr(Stdio::piped())
         .spawn()
-        .expect("failed to spawn");
+        .expect("failed to spawn command");
 
-    let stdout = cmd.stdout.as_mut().unwrap();
-    let stderr = cmd.stderr.as_mut().unwrap();
+    let child_stdout = child.stdout.as_mut().unwrap();
+    let child_stderr = child.stderr.as_mut().unwrap();
     let mut pool = Pool::new(2);
-    let output = Arc::new(Mutex::new(io::stderr()));
 
     pool.scoped(|scope| {
-        let output_stdout = output.clone();
         scope.execute(move || {
-            outputter(b"O", output_stdout, stdout);
+            outputter(b"O", child_stdout);
         });
-        let output_stderr = output.clone();
         scope.execute(move || {
-            outputter(b"E", output_stderr, stderr);
+            outputter(b"E", child_stderr);
         });
     });
 
-
-    cmd.wait().unwrap();
+    child.wait().unwrap();
 }
